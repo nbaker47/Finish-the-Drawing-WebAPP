69de4f30918ebba5bcfb855d286f8308ecd216ae
nathan <nathan.baker.2647@gmail.com>
2024-07-27 23:09:32 +0800

    compose

---
+++ infra/create_vm.sh
@@ -0,0 +1,16 @@
+gcloud compute instances create ftd-vm \
+    --project=finish-the-drawing-413709 \
+    --zone=asia-east1-c \
+    --machine-type=e2-micro \
+    --network-interface=network-tier=PREMIUM,stack-type=IPV4_ONLY,subnet=default \
+    --maintenance-policy=MIGRATE \
+    --provisioning-model=STANDARD \
+    --service-account=788234170071-compute@developer.gserviceaccount.com \
+    --scopes=https://www.googleapis.com/auth/devstorage.read_only,https://www.googleapis.com/auth/logging.write,https://www.googleapis.com/auth/monitoring.write,https://www.googleapis.com/auth/service.management.readonly,https://www.googleapis.com/auth/servicecontrol,https://www.googleapis.com/auth/trace.append \
+    --tags=http-server,https-server \
+    --create-disk=auto-delete=yes,boot=yes,device-name=ftd-vm,image=projects/debian-cloud/global/images/debian-12-bookworm-v20240709,mode=rw,size=40,type=projects/finish-the-drawing-413709/zones/asia-east1-c/diskTypes/pd-standard \
+    --no-shielded-secure-boot \
+    --shielded-vtpm \
+    --shielded-integrity-monitoring \
+    --labels=goog-ec-src=vm_add-gcloud \
+    --reservation-affinity=any
