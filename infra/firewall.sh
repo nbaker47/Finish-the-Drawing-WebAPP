b45faa6e6957a4024fea43495bacb473c1bcf642
nathan <nathan.baker.2647@gmail.com>
2024-07-28 18:58:37 +0800

    port

---
+++ infra/firewall.sh
@@ -0,0 +1,28 @@
+{
+  "allowed": [
+    {
+      "IPProtocol": "tcp",
+      "ports": [
+        "8080",
+        "3000"
+      ]
+    }
+  ],
+  "creationTimestamp": "2024-04-20T07:19:47.489-07:00",
+  "description": "",
+  "direction": "INGRESS",
+  "disabled": false,
+  "enableLogging": false,
+  "id": "8481276243244642796",
+  "kind": "compute#firewall",
+  "logConfig": {
+    "enable": false
+  },
+  "name": "custom-rule",
+  "network": "projects/finish-the-drawing-413709/global/networks/default",
+  "priority": 1000,
+  "selfLink": "projects/finish-the-drawing-413709/global/firewalls/custom-rule",
+  "sourceRanges": [
+    "0.0.0.0/0"
+  ]
+}
